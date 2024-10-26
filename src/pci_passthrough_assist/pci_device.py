71b9ee9c585afc055d33e188ba94b3ca63f01f1f
Filip Strajnar <filip.strajnar@gmail.com>
2024-10-26 19:40:49 +0200

    Added devices_in_iommu_group method.

--- src/pci_passthrough_assist/pci_device.py
+++ src/pci_passthrough_assist/pci_device.py
@@ -1,5 +1,6 @@
 from pci_passthrough_assist.pci import driver_of_pci_device
 from os.path import exists
+from os import listdir
 
 
 class PciDevice:
@@ -12,6 +13,11 @@ class PciDevice:
 
     def is_vga(self) -> bool:
         return exists(f"/sys/bus/pci/devices/{self.device_id}/boot_vga")
+
+    def devices_in_iommu_group(self) -> list['PciDevice']:
+        device_ids: list[str] = listdir(
+            f"/sys/bus/pci/devices/{self.device_id}/iommu_group/devices")
+        return [PciDevice(device_id) for device_id in device_ids]
 
     def __str__(self) -> str:
         return f"{self.device_id} driver: {self.driver_name()} VGA: {self.is_vga()}"
