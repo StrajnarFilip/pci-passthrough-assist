a29e8ba0eee916dc3d0051d7dd3e6d4680c8ee2c
Filip Strajnar <filip.strajnar@gmail.com>
2024-10-26 19:13:59 +0200

    Added method that checks if device is VGA.

--- src/pci_passthrough_assist/pci_device.py
+++ src/pci_passthrough_assist/pci_device.py
@@ -1,4 +1,5 @@
 from pci_passthrough_assist.pci import driver_of_pci_device
+from os.path import exists
 
 
 class PciDevice:
@@ -8,3 +9,9 @@ class PciDevice:
 
     def driver_name(self) -> str:
         return driver_of_pci_device(self.device_id)
+
+    def is_vga(self) -> bool:
+        return exists(f"/sys/bus/pci/devices/{self.device_id}/boot_vga")
+
+    def __str__(self) -> str:
+        return f"{self.device_id} driver: {self.driver_name()} VGA: {self.is_vga()}"
