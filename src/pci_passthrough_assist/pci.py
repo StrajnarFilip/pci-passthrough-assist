548527c302d9c4096d6e0dd233774904ca997b73
Filip Strajnar <filip.strajnar@gmail.com>
2024-10-26 18:43:56 +0200

    Added a function that gets name of PCI device's driver.

--- src/pci_passthrough_assist/pci.py
+++ src/pci_passthrough_assist/pci.py
@@ -1,4 +1,5 @@
 from os import listdir
+from os.path import realpath, basename
 
 
 def all_pci_device_ids() -> list[str]:
@@ -7,3 +8,9 @@ def all_pci_device_ids() -> list[str]:
 
 def all_pci_driver_ids() -> list[str]:
     return listdir("/sys/bus/pci/drivers")
+
+
+def driver_of_pci_device(pci_device_id: str) -> str:
+    driver_directory: str = realpath(
+        f"/sys/bus/pci/devices/{pci_device_id}/driver")
+    return basename(driver_directory)
